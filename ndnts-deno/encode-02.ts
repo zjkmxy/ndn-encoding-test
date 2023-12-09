af04ce9cfbf607990fe9c7fda3d0894d37a4da5c
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2023-12-09 02:58:40 -0800

    Add NDNts code

---
+++ ndnts-deno/encode-02.ts
@@ -0,0 +1,25 @@
+import { Name, Data } from "@ndn/packet"
+import { Encoder } from "@ndn/tlv"
+
+const oneRun = async (i: number) => {
+  const nameStr = "/go-ndn/test/" + i.toLocaleString('en-US', { minimumIntegerDigits: 10, useGrouping: false })
+  const content = new Uint8Array(4000)
+  const data = new Data(
+    new Name(nameStr),
+    Data.ContentType(0),
+    Data.FreshnessPeriod(4000),
+    Data.FinalBlock,
+    "10000",
+    content
+  )
+  Encoder.encode(data)
+}
+
+if (import.meta.main) {
+  const tic = performance.now()
+  for (let i = 0; i < 1000000; i++) {
+    oneRun(i)
+  }
+  const toc = performance.now()
+  console.log(`Total time: ${(toc - tic) / 1000.0} seconds`)
+}
